d62d244984ffb5f1cb12c49c569535af5e90a19b
cobalt74 <cobalt74@gmail.com>
2014-01-27 18:03:30 +0100

    V1.1 plugin ScrollRead - Amélioration de l’expérience utilisateur (enfin je l’espère) :) https://github.com/ldleman/Leed/issues/80

--- scrollRead/js/scrollRead.js
+++ scrollRead/js/scrollRead.js
@@ -1,4 +1,79 @@
+/*
+ @nom: scrollRead
+ @auteur: Maël ILLOUZ (mael.illouz@cobestran.com)
+ @description: Js de gestion du scroll pour la lecture automatique des articles.
+ */
+
 $(window).data('plugin_scroll_read', true);
+
+// Remplacement de la fonction existante de Leed.
+// A faire évoluer en fonction des évolutions et corrections de la fonction dans Leed
+var oldReadThis = readThis;
+readThis = function(element,id,from,callback) {
+    var activeScreen = $('#pageTop').html();
+    var parent = $(element).parent().parent();
+    var nextEvent = $('#'+id).next();
+    //sur les éléments non lus
+    if(!parent.hasClass('eventRead')){
+        $.ajax({
+            url: "./action.php?action=readContent",
+            data:{id:id},
+            success:function(msg){
+                if(msg.status == 'noconnect') {
+                    alert(msg.texte)
+                } else {
+                    if( console && console.log && msg!="" ) console.log(msg);
+                    switch (activeScreen){
+                        case '':
+                            // cas de la page d'accueil
+                            parent.addClass('eventRead');
+                            if(callback){ callback(); }
+                            // on simule un scroll si tous les events sont cachés
+                            /*if($('article section:last').attr('style')=='display: none;') {
+                                $(window).scrollTop($(document).height());
+                            }*/
+                            // on compte combien d'article ont été lus afin de les soustraires de la requête pour le scroll infini
+                            $(window).data('nblus', $(window).data('nblus')+1);
+                            // on diminue le nombre d'article en haut de page
+                            $('#nbarticle').html(parseInt($('#nbarticle').html()) - 1)
+                            break;
+                        case 'selectedFolder':
+                            parent.addClass('eventRead');
+                            if(callback){ callback(); }
+                            // on compte combien d'article ont été lus afin de les soustraires de la requête pour le scroll infini
+                            $(window).data('nblus', $(window).data('nblus')+1);
+                            break;
+                        default:
+                            // autres cas : favoris, selectedFeed ...
+                            parent.addClass('eventRead');
+                            if(callback){ callback(); }
+                            break;
+                    }
+                }
+            }
+        });
+    }else{  // sur les éléments lus
+        // si ce n'est pas un clic sur le titre de l'event
+        if(from!='title'){
+            $.ajax({
+                url: "./action.php?action=unreadContent",
+                data:{id:id},
+                success:function(msg){
+                    if(msg.status == 'noconnect') {
+                        alert(msg.texte)
+                    } else {
+                        if( console && console.log && msg!="" ) console.log(msg);
+                        parent.removeClass('eventRead');
+                        // on compte combien d'article ont été remis à non lus
+                        if ( (activeScreen=='') || (activeScreen=='selectedFolder') ) $(window).data('nblus', $(window).data('nblus')-1);
+                        if(callback){ callback(); }
+                    }
+                }
+            });
+        }
+    }
+}
+
 $(window).scroll(function(){
     scrollRead();
 });
@@ -17,6 +92,7 @@ jQuery.fn.extend({
     }
 });
 
+// fonction principal de gestion du scroll
 function scrollRead(){
 
     // on arrête la fonction lorsqu'on fait le changement d'article
@@ -24,19 +100,32 @@ function scrollRead(){
 
     posEventSelected = $('.eventSelected').findPos();
     heightEventSelected = $('.eventSelected').height();
-    pos_next_article = heightEventSelected + posEventSelected.y - 10;
+    pos_next_article = heightEventSelected + posEventSelected.y;
 
-    /*
-    console.info('pos_next_article : '+ pos_next_article);
-    console.info('windows ScrollTop : '+$(window).scrollTop());
+    //console.info('posEventSelected.y : ' + posEventSelected.y);
+    //console.info('pos_next_article : '+ pos_next_article);
+    console.info('windows ScrollTop : '+ $(window).scrollTop());
     console.info('height:'+$(document).height());
-    */
+    console.info('bas:'+ ($(window).scrollTop() + $(window).height()));
 
-    if (pos_next_article < $(window).scrollTop())
+    if ($(window).data('ajaxready') == false
+            && $(window).scrollTop()<50
+            && $('article #loader').style == 'display: none;') return;
+    var deviceAgent = navigator.userAgent.toLowerCase();
+    var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+    if(($(window).scrollTop() + $(window).height())  == $(document).height()
+        || agentID && ($(window).scrollTop() + $(window).height()) + 150 > $(document).height())
+    {
+        $('article section').last().height ($(window).height());
+    }
+
+    // je déclanche uniquement sur la ligne virtuelle située en dessous du titre d'un article.
+    if (posEventSelected.y + 20 < $(window).scrollTop())
     {
         $(window).data('plugin_scroll_read', false);
 
         /*
+        console.info('posEventSelected.y : ' + posEventSelected.y);
         console.info('pos_next_article : '+ pos_next_article);
         console.info('windows ScrollTop : '+$(window).scrollTop());
         console.info('height:'+$(document).height());
@@ -47,10 +136,12 @@ function scrollRead(){
         var parent = $(buttonElement).parent().parent();
         var tempo = true;
         //console.info(parent);
+        // élément lu?
         if (parent.hasClass('eventRead'))
         {
             //passage à l'élément suivant
-            targetNextEvent();
+            //targetNextEvent();
+            targetThisEvent($('.eventSelected').next(':visible'),false);
             $(window).data('plugin_scroll_read', true);
         }
         else
@@ -60,7 +151,7 @@ function scrollRead(){
             tempo = false;
             readThis(buttonElement,id,null,function(){
                 // on fait un focus sur l'Event suivant
-                targetThisEvent($('.eventSelected').next(),true);
+                targetThisEvent($('.eventSelected').next(),false);
                 $(window).data('plugin_scroll_read', true);
             });
         }
